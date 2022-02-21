c8c61bcaab9c2903c6983196fe276211d39337b2
goldstar611 <goldstar611@users.noreply.github.com>
2022-02-21 17:30:41 -0600

    Pycharm reformat

--- setup.py
+++ setup.py
@@ -8,41 +8,37 @@ import sys
 
 from setuptools import setup
 
-
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-
 with open('gitless/cli/gl.py', 'rb') as f:
-  version = str(ast.literal_eval(_version_re.search(
-    f.read().decode('utf-8')).group(1)))
-
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
 
 # Build helper
 if sys.argv[-1] == 'gl-build':
-  from subprocess import run
-  import shutil
-  import tarfile
-  import platform
+    from subprocess import run
+    import shutil
+    import tarfile
+    import platform
 
-  rel = 'gl-v{0}-{1}-{2}'.format(
-      version, platform.system().lower(), platform.machine())
+    rel = 'gl-v{0}-{1}-{2}'.format(
+        version, platform.system().lower(), platform.machine())
 
-  print('running pyinstaller...')
-  run(
-    ['pyinstaller', 'gl.spec', '--clean', '--distpath', rel],
-    stdout=sys.stdout, stderr=sys.stderr)
-  print('success!! gl binary should be at {0}/gl'.format(rel))
+    print('running pyinstaller...')
+    run(
+        ['pyinstaller', 'gl.spec', '--clean', '--distpath', rel],
+        stdout=sys.stdout, stderr=sys.stderr)
+    print('success!! gl binary should be at {0}/gl'.format(rel))
 
-  print('creating tar.gz file')
-  shutil.copy('README.md', rel)
-  shutil.copy('LICENSE.md', rel)
+    print('creating tar.gz file')
+    shutil.copy('README.md', rel)
+    shutil.copy('LICENSE.md', rel)
 
-  with tarfile.open(rel + '.tar.gz', 'w:gz') as tar:
-    tar.add(rel)
-  print('success!! binary release at {0}'.format(rel + '.tar.gz'))
+    with tarfile.open(rel + '.tar.gz', 'w:gz') as tar:
+        tar.add(rel)
+    print('success!! binary release at {0}'.format(rel + '.tar.gz'))
 
-  sys.exit()
-
+    sys.exit()
 
 ld = """
 Gitless is a version control system built on top of Git, that is easy to learn
@@ -65,9 +61,9 @@ setup(
     url='https://gitless.com',
     packages=['gitless', 'gitless.cli'],
     install_requires=[
-      # make sure install_requires is consistent with requirements.txt
-      'pygit2==1.4.0', # requires libgit2 1.1.x
-      'argcomplete>=1.11.1'
+        # make sure install_requires is consistent with requirements.txt
+        'pygit2==1.4.0',  # requires libgit2 1.1.x
+        'argcomplete>=1.11.1'
     ],
     license='MIT',
     classifiers=[
