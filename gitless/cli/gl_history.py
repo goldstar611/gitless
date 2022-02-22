62c652ce04eaa8385667956ec491c46a43d9bdca
goldstar611 <goldstar611@users.noreply.github.com>
2022-02-21 18:27:32 -0600

    - alias `gl log` to `gl history`

--- gitless/cli/gl_history.py
+++ gitless/cli/gl_history.py
@@ -14,7 +14,7 @@ def parser(subparsers, _):
     """Adds the history parser to the given subparsers object."""
     desc = 'show commit history'
     history_parser = subparsers.add_parser(
-        'history', help=desc, description=desc.capitalize(), aliases=['hs'])
+        'history', help=desc, description=desc.capitalize(), aliases=['hs', 'log'])
     history_parser.add_argument(
         '-v', '--verbose', help='be verbose, will output the diffs of the commit',
         action='store_true')
