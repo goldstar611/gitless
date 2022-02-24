072f418540bc900fdb761f80bbcc3f7c8ba998d7
goldstar611 <goldstar611@users.noreply.github.com>
2022-02-23 19:28:55 -0600

    Adds `gl fetch`, a wrapper for `git fetch`

---
+++ gitless/cli/gl_fetch.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Gitless - a version control system built on top of Git
+# Licensed under MIT
+
+"""gl fetch - A wrapper around git-fetch(1)."""
+
+
+from ..import core
+
+
+def parser(subparsers, _):
+    """Adds the fetch parser to the given subparsers object."""
+    desc = 'Synchronize branches, tags, references and other meta data from another repository.\n' \
+           'For more information on this advanced command refer to the manual page for git-fetch.'
+    fetch_parser = subparsers.add_parser(
+        'fetch', help=desc, description=desc.capitalize(), aliases=['ft'])
+    fetch_parser.set_defaults(func=main)
+    fetch_parser.add_argument(
+        'fetch_args', nargs="*", help='Additional arguments to pass to `git fetch`')
+
+
+def main(args, repo):
+    core.git('fetch', *args.fetch_args)
